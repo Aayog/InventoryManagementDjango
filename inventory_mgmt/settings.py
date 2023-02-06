cb70eceff85174cad11e554e88b10bc6de3ea1cb
Aayog Koirala <1aayogkoirala@gmail.com>
2023-02-05 22:43:15 -0500

    Mostly working, except notif

--- inventory_mgmt/settings.py
+++ inventory_mgmt/settings.py
@@ -125,4 +125,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST_USER ="rudahabmar@gmail.com"
-EMAIL_HOST_PASSWORD ="2UbEYyZk7bUnHfX"
+EMAIL_HOST_PASSWORD =""
